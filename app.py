55dfb68731a2237254bc84ca3820aae35b8c078f
ericekotto <129702224+ericekotto@users.noreply.github.com>
2026-02-11 05:23:51 +0100

    Update welcome message on homepage

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ projet = st.sidebar.radio("Sélectionnez un projet :",
 
 # --- PAGE D'ACCUEIL ---
 if projet == "Accueil":
-    st.title("🚀 BIENVENU SUR L'INTERFACE DE TEST DE NOS 3 MODELES DE MACHINE DONT LES DESCRIPTIONS SONT DONNEES CI-DESSOUS")
+    st.title("TEST DE NOS 3 MODELES DE MACHINE DONT LES DESCRIPTIONS SONT DONNEES CI-DESSOUS?SOYEZ LA BIENVENUE")
     st.markdown("""
     Bienvenue dans cette interface de démonstration. Cette application regroupe trois modèles de Machine Learning 
     distincts, illustrant des cas d'usage concrets en entreprise : **Classification socio-économique**, 
